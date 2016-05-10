e84fe63efccbb617f52e146f58706e296db217a6
Emily Mearns <mearnsem@gmail.com>
2016-05-10 09:54:01 -0600

    Stretch Problem

--- StretchProblem/FindNumbersDivisibleByX.playground/Contents.swift
+++ StretchProblem/FindNumbersDivisibleByX.playground/Contents.swift
@@ -2,4 +2,25 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//Make a method that prints every X number between 0 and Y
+//take in x and y, return all numbers between 0 and y that can be divided by x
+
+/*
+ 
+ return 0...y % x == 0
+ 
+ */
+
+func divisibleBy(x: Int, y: Int) {
+    let intArray = Array(0...y)
+    for integer in intArray {
+        if (integer % x == 0) {
+            print(integer)
+        }
+    }
+}
+
+divisibleBy(2, y: 20)
+
+
+
